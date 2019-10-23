f4e850bbfbeb38b3f8978428983acff4e91526ff
ShorinaMaria <maryss11@yandex.ru>
2019-10-23 22:48:13 +0300

    Учебный проект: валидируй, как никогда

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -13,8 +13,8 @@ const App = (props) => {
 };
 
 App.propTypes = {
-  gameTime: PropTypes.number,
-  errorCount: PropTypes.number
+  gameTime: PropTypes.number.isRequired,
+  errorCount: PropTypes.number.isRequired
 };
 
 export default App;
